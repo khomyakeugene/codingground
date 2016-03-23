9c3a8182451af2b999ead8f68c848bce11019652
khomyakeugene <khomyak_eugene@yahoo.com>
2016-03-23 20:52:07 +0200

    Created by Coding Ground Client

---
+++ New Project-20160323/Main.java
@@ -0,0 +1,8 @@
+
+import collections.CollectionEfficiencyDemonstrator;
+
+public class Main {
+    public static void main(String[] args) {
+        new CollectionEfficiencyDemonstrator().demonstrate();
+    }
+}
